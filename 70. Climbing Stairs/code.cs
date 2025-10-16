fb4d5c3605badb9774a828c2207e6dc869b11df5
chris <jiulrulsmu@gmail.com>
2025-10-16 20:37:01 +0800

    70. Climbing Stairs

---
+++ 70. Climbing Stairs/code.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if(n <= 3) return n;
+
+        var dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i = 4; i <= n; i++)
+        {
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+        return dp[n];
+    }
+}
